f4e8ca65e3effa1424da08c97171b3968c9d5ca3
Developer <m_mannaa2008@yahoo.com>
2020-09-22 12:56:51 +0300

    Add the Models and show the app in my admin control panel

---
+++ units/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Unit, Furniture, Country, City, Area
+
+# Register your models here.
+admin.site.register(Unit)
+admin.site.register(Furniture)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Area)
