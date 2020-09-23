106cede9b4b53ffb97be16c5f7e93092c5461a97
Developer <m_mannaa2008@yahoo.com>
2020-09-23 10:18:14 +0300

    Add some Fields & data to the Units

--- units/admin.py
+++ units/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Unit, Furniture, Country, City, Area
+from .models import Unit, Furniture, Country, City, Area, Room
 
 # Register your models here.
 admin.site.register(Unit)
 admin.site.register(Furniture)
+admin.site.register(Room)
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Area)
--- units/models.py
+++ units/models.py
@@ -13,6 +13,15 @@ UNIT_TYPE = (
     ('Lounge' , 'Lounge'),
 )
 
+UNIT_STATUS = (
+    ('AVAILABLE TO RENT','AVAILABLE TO RENT'),
+    ('RENTED' , 'RENTED'),
+    ('AVAILABLE TO SELL','AVAILABLE TO SELL'),
+    ('SOLD' , 'SOLD'),
+    ('SUSPENDED' , 'SUSPENDED'),
+
+)
+
 # Area Table
 class Area(models.Model):
     title = models.CharField(max_length=60, verbose_name='Area', )
@@ -44,6 +53,13 @@ class Furniture(models.Model):
     def __str__(self):
         return self.title
 
+# Rooms Table
+class Room(models.Model):
+    title = models.CharField(max_length=60, verbose_name='Room', )
+    
+    def __str__(self):
+        return self.title        
+
 # Main Unite Table
 class Unit(models.Model):
 
@@ -53,15 +69,18 @@ class Unit(models.Model):
     num_bedrooms = models.IntegerField( verbose_name='Number of Bed rooms') # Count of Closed Rooms
     hole_space = models.IntegerField( verbose_name='Hole Space') # Count of Closed Rooms
     #image = models.ImageField(upload_to='units_images/' , blank=True , null=True, verbose_name='Unit Main Image') # Internal Images
-    created_at = models.DateTimeField(default=timezone.now) # Post Creation Date
     active = models.BooleanField(default=True) # Post Status
     owner_email = models.EmailField(default='m.medhat@dropsgroup.com') # Email
     type = models.CharField(choices=UNIT_TYPE , default='FLAT',max_length=20) # Unite Type
+    status = models.CharField(choices=UNIT_STATUS , default='SUSPENDED',max_length=20) # Unite Status
     country = models.ForeignKey(Country, on_delete=models.CASCADE) # Country
     city = models.ForeignKey(City, on_delete=models.CASCADE) # City
     area = models.ForeignKey(Area, on_delete=models.CASCADE) # Area
-    #furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE) # Furniture Type
-    furniture = models.ManyToManyField(Furniture)
+    #furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE) 
+    furniture = models.ManyToManyField(Furniture) # Room Type
+    Room = models.ManyToManyField(Room) # Room Type
+    remarks = models.TextField(max_length=500, verbose_name='Remarks') # Remarks
+    created_at = models.DateTimeField(default=timezone.now) # Post Creation Date
 
     def __str__(self):
         return self.title       
