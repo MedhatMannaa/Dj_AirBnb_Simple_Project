1ddfe9ddaa702ca540401e7898c31f2cedb0520f
Developer <m_mannaa2008@yahoo.com>
2020-09-24 13:01:59 +0300

    save but views not complete

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from units.views import all_units , single_unit
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('unit/', include('units.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- units/views.py
+++ units/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Unit
+   
+#class Meta:
+#    db_table = ''
+#        verbose_name=''
+#        verbose_name_plural=verbose_name
+#    def __str__(self):
+#        return self.
 
 # Create your views here.
+
+def all_units(request):
+	## Logic to get all units in one html page
+	all_units = Unit.objects.all()
+	return render(request,'unit/all_units.html' ,{'units':all_units})
+		
+	## unit/all_units.html = save place/html page name which saved in this place
+	## units               = name which we will send to the FrontEnd
+	## all_units           = Name of the Variable
+
+def single_unit(request,id):
+	## Logic to get all units in one html page
+	single_unit = Unit.objects.get(id=id)
+	return render(request,'unit/single_unit.html' ,{'unit':single_unit})
+		
+	## unit/single_unit.html = save place/html page name which saved in this place
+	## unit                  = name which we will send to the FrontEnd
+	## single_unit           = Name of the Variable    
+
+
+
+
